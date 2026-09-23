bf70fc5b06d931e4cecd0dec607f32dd1f0bd304
ratheesh025 <ratheesh25r@gmail.com>
2026-09-23 18:44:18 +0530

    delete-php

--- api/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/bootstrap.php';
-
-if (!current_user_id()) {
-    respond(true, ['loggedIn' => false]);
-}
-
-respond(true, ['loggedIn' => true, 'user' => ['id' => $_SESSION['user_id'], 'name' => $_SESSION['user_name']]]);
--- api/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/bootstrap.php';
-$_SESSION = [];
-session_destroy();
-respond(true, ['message' => 'Signed out']);
